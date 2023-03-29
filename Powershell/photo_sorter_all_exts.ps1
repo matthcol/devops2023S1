4922daffe95774c892ff4914d6bd90d2f13b402f
matthcol <matthias.colin@gmail.com>
2023-03-29 14:44:36 +0200

    fix regexp

--- Powershell/photo_sorter_all_exts.ps1
+++ Powershell/photo_sorter_all_exts.ps1
@@ -21,34 +21,33 @@ if(!(Test-Path "$dest_dir" -PathType Container)) {
 }
 
 Write-Output "Scan directory [$src_dir] for photos..."
-
-Write-Debug "Looking for photos with extension $ext"
 Write-Debug "Source Directory: $src_dir"
 Write-Debug "Destination Directory: $dest_dir"
-$img_format = "^IMG_[0-9]{8}_.*$ext$"
+
+$img_format = "^IMG_[0-9]{8}_"
 
 foreach ($img in Get-ChildItem -Path "$src_dir" | Where-Object Extension -In $exts) {
     $img_filename = $img.Name
     Write-Debug "About to process image: $img_filename"
-    if ($img_filename -match  $img_format){
-        Write-Debug "Good format"
-        $tag_year = $img_filename.Substring(4,4)
-        $tag_month = $img_filename.Substring(8,2)
-        $tag_day = $img_filename.Substring(10,2)
-        Write-Debug "y: $tag_year, m: $tag_month, d: $tag_day"
-        $dest_img_dir = Join-Path "$dest_dir" "$tag_year" "$tag_month" "$tag_day"
-        Write-Debug "will be store in dir: $dest_img_dir"
-        if (-not (Test-Path $dest_img_dir)) {
-            # mkdir
-            New-Item -ItemType Directory $dest_img_dir | Out-Null
-        }
-        $dest_img_path = Join-Path "$dest_img_dir" "$img_filename"
-        if (Test-Path $dest_img_path) {
-            Write-Output "Skip photo [${img_filename}]: already exists in $dest_img_dir"
-            continue
-        }
-        Move-Item -Path $img -Destination $dest_img_path
-    } else {
+    if (-not ($img_filename -match  $img_format)){
         Write-Output "Skip photo [${img_filename}]: wrong format"
+        continue # go to next photo
     }
-}
+    Write-Debug "Good format"
+    $tag_year = $img_filename.Substring(4,4)
+    $tag_month = $img_filename.Substring(8,2)
+    $tag_day = $img_filename.Substring(10,2)
+    Write-Debug "y: $tag_year, m: $tag_month, d: $tag_day"
+    $dest_img_dir = Join-Path "$dest_dir" "$tag_year" "$tag_month" "$tag_day"
+    Write-Debug "will be store in dir: $dest_img_dir"
+    if (-not (Test-Path $dest_img_dir)) {
+        # mkdir
+        New-Item -ItemType Directory $dest_img_dir | Out-Null
+    }
+    $dest_img_path = Join-Path "$dest_img_dir" "$img_filename"
+    if (Test-Path $dest_img_path) {
+        Write-Output "Skip photo [${img_filename}]: already exists in $dest_img_dir"
+        continue # go to next photo (next iteration)
+    }
+    Move-Item -Path $img -Destination $dest_img_path
+} # end foreach
