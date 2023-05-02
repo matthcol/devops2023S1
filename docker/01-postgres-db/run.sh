963e400158d2e63dc8b4f49c07a1b424bd2b4b52
matthcol <matthias.colin@gmail.com>
2023-05-02 11:29:50 +0200

    db and user custom

--- docker/01-postgres-db/run.sh
+++ docker/01-postgres-db/run.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 TAG=15.2
 NAME=postgres_dbmovie
+POSTGRES_USER=movie
 POSTGRES_PASSWORD=password
 POSTGRES_PORT=5432
+POSTGRES_DB=dbmovie
 
 docker run -p $POSTGRES_PORT:5432 \
+	-e "POSTGRES_USER=$POSTGRES_USER" \
 	-e "POSTGRES_PASSWORD=$POSTGRES_PASSWORD" \
+	-e "POSTGRES_DB=$POSTGRES_DB" \
 	--name $NAME \
 	-d postgres:$TAG
